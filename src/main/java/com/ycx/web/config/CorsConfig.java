9fd0039b2cb7ebf6477955af29f0d0b3cf7d0abd
chencay <yuchengxin3231@126.com>
2020-04-25 16:44:18 +0800

    [add]增加跨域配置 + 数据库配置

--- src/main/java/com/ycx/web/config/CorsConfig.java
+++ src/main/java/com/ycx/web/config/CorsConfig.java
@@ -1,9 +1,26 @@
 package com.ycx.web.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 /**
  * @author ycx
  * @description 描述
  * @date 2020/4/219:49 下午
  **/
-public class CorsConfig {
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    /** * **/
+    private static final String ALL = "*";
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+        corsRegistry.addMapping("/**")
+                .allowedOrigins(ALL)
+                .allowedMethods(ALL)
+                .allowedHeaders(ALL)
+                .allowCredentials(true)
+                .maxAge(1800);
+    }
 }
